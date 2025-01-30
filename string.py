cd3e0e9aaa4421b283fdc03076815e0a7dba474a
harsikatrk <harsika272@gmail.com>
2025-01-30 17:46:40 +0530

    string

---
+++ string.py
@@ -0,0 +1,8 @@
+var3="hiran ram babu"
+print(var3.capitalize())
+print(var3.lower())
+print(var3.upper())
+print(var3.count('a'))
+print(var3.join(['',' is my name']))
+print(var3.replace(' ','_'))
+print(var3.split())
