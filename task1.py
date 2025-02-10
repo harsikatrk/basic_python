781462ea7dace3da9d728267295030200b90f2eb
harsikatrk <harsika272@gmail.com>
2025-02-10 22:26:04 +0530

    update task1

--- task1.py
+++ task1.py
@@ -1,25 +1,25 @@
 
-num1=int(input("enter first number:"))
-num2=int(input("enter second number:"))
-result=0
+num1=int(input("Enter first number   :")) #to enter first number
+num2=int(input("Enter second number :")) #to enter second number
+value=input("Enter any operation +, -, *, /, % :")  # to enter the necessary operator
 
-value=input("enter any operation +, -, *, /, % :")
+result=0  #initialize result to zero
 
 if value == '+' :
-    result=num1+num2
-    print("Addition of {}and {} is".format(num1,num2),result)
+    result=num1+num2  #addition 
+    print("Addition of {}and {} is".format(num1,num2),result) #to print the result in desired form
 elif value == '-':
-    result=num1-num2
-    print("Subtraction of {}and {} is".format(num1,num2),result)
+    result=num1-num2 #subtraction
+    print("Subtraction of {}and {} is".format(num1,num2),result) #to print the result in desired form
 elif value == '*':
-    result=num1*num2
-    print("Multiplication of {}and {} is".format(num1,num2),result)
+    result=num1*num2 #multiplication
+    print("Multiplication of {}and {} is".format(num1,num2),result) #to print the result in desired form
 elif value == '/':
-    result = num1/num2
-    print("Division of {}and {} is".format(num1,num2),result)
+    result = num1/num2 #division
+    print("Division of {}and {} is".format(num1,num2),result) #to print the result in desired form
 elif value =='%':
-    result = num1%num2
-    print("Modulus of {}and {} is".format(num1,num2),result)
+    result = num1%num2 #modulus
+    print("Modulus of {}and {} is".format(num1,num2),result) #to print the result in desired form
 else:
-    print("no such operation!!!")
+    print("no such operation!!!") #to print the result in desired form
 
